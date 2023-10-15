e40c0a638914a91cf953216fc073de71e33ea75c
Ramothole Kagiso <32358546+Ramothole@users.noreply.github.com>
2023-10-15 15:03:34 +0200

    chore: added background image

--- lib/Question2/home_redux.dart
+++ lib/Question2/home_redux.dart
@@ -2,7 +2,6 @@ import 'package:CounterApp/Question2/counter_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-
 class ReduxCounterScreen extends StatelessWidget {
   const ReduxCounterScreen({super.key});
 
@@ -10,54 +9,61 @@ class ReduxCounterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreProvider<int>(
       store: store,
-      
-      child: Scaffold(
-     
-      appBar: AppBar(
-        title: const Text('Redux Pattern'),
-      ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              StoreConnector<int, int>(
-              converter: (store) => store.state,
-              builder: (context, count) {
-                return Text(
-                  '$count', // Display the count as a string
-                  style: TextStyle(fontSize: 48.0),
-                );
-              },
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/background.jpeg"), fit: BoxFit.cover),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text('Redux Pattern'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                StoreConnector<int, int>(
+                  converter: (store) => store.state,
+                  builder: (context, count) {
+                    return Text(
+                      '$count',
+                      style:
+                          const TextStyle(fontSize: 48.0, color: Colors.white),
+                    );
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    StoreConnector<int, VoidCallback>(
+                      converter: (store) {
+                        return () => store.dispatch(ActionType.increment);
+                      },
+                      builder: (context, callback) {
+                        return ElevatedButton(
+                          onPressed: callback,
+                          child: const Text('ADD'),
+                        );
+                      },
+                    ),
+                    StoreConnector<int, VoidCallback>(
+                      converter: (store) {
+                        return () => store.dispatch(ActionType.decrement);
+                      },
+                      builder: (context, callback) {
+                        return ElevatedButton(
+                          onPressed: callback,
+                          child: const Text('MINUS'),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  StoreConnector<int, VoidCallback>(
-                    converter: (store) {
-                      return () => store.dispatch(ActionType.increment);
-                    },
-                    builder: (context, callback) {
-                      return ElevatedButton(
-                        onPressed: callback,
-                        child: Text('+'),
-                      );
-                    },
-                  ),
-                  StoreConnector<int, VoidCallback>(
-                    converter: (store) {
-                      return () => store.dispatch(ActionType.decrement);
-                    },
-                    builder: (context, callback) {
-                      return ElevatedButton(
-                        onPressed: callback,
-                        child: Text('-'),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ],
           ),
         ),
       ),
